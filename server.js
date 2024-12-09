9d0cfb1248f5dd750a43a786471c073df8b02154
Jonathan Sage <sagejonathan.tesol@gmail.com>
2024-12-08 17:12:09 -0700

    undo cors setting

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 // Middleware
-// app.use(cors());
-app.use(cors({
-  origin: [
-    'https://solarwatch-server.vercel.app',
-    `http://localhost:${port}`, 
-  ]
-}));
+app.use(cors());
+// app.use(cors({
+//   origin: [
+//     'https://solarwatch-server.vercel.app',
+//     `http://localhost:${port}`, 
+//   ]
+// }));
 app.use(express.static("public"));
 app.use(express.json());
 
